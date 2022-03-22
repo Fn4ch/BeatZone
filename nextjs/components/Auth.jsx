82799d5e32e10c9eefa20ea958641fbd7acb22bd
Fn4ch <farhadmainsot@gmail.com>
2022-03-22 19:44:14 +0300

    authPage
    исправлена страница авторизации

--- nextjs/components/Auth.jsx
+++ nextjs/components/Auth.jsx
@@ -1,44 +1,27 @@
-import React,{useState, useContext} from 'react'
-import './Auth.css'
-import axios from 'axios'
-import {AuthContext} from '../context/AuthContext'
-import { response } from 'express'
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
-import { Grid, GridItem, TextField, Typography, Button} from '@material-ui/core'
-import { createTheme, makeStyles} from '@material-ui/styles'
+import {useState} from 'react'
+import { Grid, TextField, Typography, Button, Container} from '@mui/material'
+import { createTheme} from '@mui/material'
+import Paper from '@mui/material/Paper'
+import { experimentalStyled as styled } from '@mui/material/styles'
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 function Auth(){
     const [form, setForm] = useState({
         username : '',
         password : ''
       })
-      
-      const {login} = useContext(AuthContext)
-      
+            
       const changeHandler = (event) => {
         setForm({...form,[event.target.name]: event.target.value})
         console.log(form)
       }
-
-      const loginHandler = async () => {
-        await axios.post('/login', {...form},
-            {
-              headers : {
-                'Content-Type': 'application/json'
-              }
-            }).then(res=> {login(response.data.token, response.data.userId)})
-            .catch(error=>console.log(error))
-          }
-
-      const registerHandler = async () => {
-        await axios.post('/registration', {...form},
-            {
-              headers : {
-                'Content-Type': 'application/json'
-              }
-            }).then(res=>console.log(res))
-            .catch(error=>console.log(error))
-          }
 
        const theme = createTheme({
     palette: {
@@ -57,46 +40,46 @@ function Auth(){
     },
   });
 
+  const loginHandler = () =>{
+
+  }
+  const registerHandler = () =>{
+    
+  }
 
 return(
-    <BrowserRouter>
-    <Switch>        
-        <Route path="/login">
-          <Grid fixed>
-          <GridItem>
-            <Typography color='textPrimary'>Авторизация</Typography>
-          </GridItem>
-          <GridItem>
-            <TextField onChange={changeHandler} id="filled-search" variant='outlined' label='Логин' size='small' fullWidth='100'>
-            </TextField>
-          </GridItem>
-          <GridItem>
-            <TextField onChange={changeHandler} id="filled-search" variant='outlined' label='Пароль' size='small' fullWidth='100'>
-            </TextField>
-          </GridItem>
-          <Button onClick={loginHandler} color='secondary'>Войти</Button>
+        <> <Container maxWidth="sm">
+            <Grid fixed>
+            <Item>
+            <Typography color='primary' variant="h2">Авторизация</Typography>
+            </Item>
+            <Item>
+              <TextField onChange={changeHandler} id="filled-search" variant='outlined' label='Логин' size='small' fullWidth='100'>
+              </TextField>
+            </Item>
+            <Item>
+              <TextField onChange={changeHandler} id="filled-search" variant='outlined' label='Пароль' size='small' fullWidth='100'>
+              </TextField>
+            </Item>
+            <Button onClick={loginHandler} color='secondary'>Войти</Button>
           </Grid>
-        </Route>
-          
-
-      <Route path="/registration">
+               
           <Grid fixed>
-          <GridItem>
-            <Typography color='textPrimary'>Регистрация</Typography>
-          </GridItem>
-          <GridItem>
-            <TextField onChange={changeHandler} id="filled-search" variant='outlined' label='Логин' size='small' fullWidth='100'>
-            </TextField>
-          </GridItem>
-          <GridItem>
-            <TextField onChange={changeHandler} id="filled-search" variant='outlined' label='Пароль' size='small' fullWidth='100'>
-            </TextField>
-          </GridItem>
-          <Button onClick={registerHandler} color='secondary'>Зарегистрироваться</Button>
+            <Item>
+              <Typography color='primary' variant="h2">Регистрация</Typography>
+            </Item>
+            <Item>
+              <TextField onChange={changeHandler} id="filled-search" variant='outlined' label='Логин' size='small' fullWidth='100'>
+              </TextField>
+            </Item>
+            <Item>
+              <TextField onChange={changeHandler} id="filled-search" variant='outlined' label='Пароль' size='small' fullWidth='100'>
+              </TextField>
+            </Item>
+            <Button onClick={registerHandler} color='secondary'>Зарегистрироваться</Button>
           </Grid>
-      </Route>
-    </Switch>
-    </BrowserRouter>
+          </Container>
+        </>
     )
 }
 export default Auth
